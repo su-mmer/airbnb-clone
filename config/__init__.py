70e8d3c3d80a1a147ad03cf0caa2e47b29c2bd61
su-mmer <heekim987@gmail.com>
2019-11-09 03:20:34 +0900

    lecture finished 2.4, learn migrate, git ignore, pipfilel..

---
+++ config/__init__.py
@@ -0,0 +1,7 @@
+# 파이썬에게 필요한 파일.
+# 새로운 폴더를 만들 때, 외부에서 그 폴더의 파일들을 써야 할 때는 새폴더 안에 __init__.py를 두어야 함
+
+
+# You have 17 unapplied migration(s). Your project may not work properly until you apply the migrations for app(s): admin, auth, contenttypes, sessions.
+# Run 'python manage.py migrate' to apply them.
+# db가 migrate(연동) 되지 않았기 때문에 나타나는 에러
